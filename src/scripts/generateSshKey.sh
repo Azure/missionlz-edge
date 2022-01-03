4523e7e685233fd10b6ec356a27c417d6e340c1a
Vidya Bala <bsailesh@msn.com>
2022-01-03 13:15:20 -0500

    updated the path of key file

--- src/scripts/generateSshKey.sh
+++ src/scripts/generateSshKey.sh
@@ -3,4 +3,4 @@ echo -e \'y\' | ssh-keygen -f scratch -N ""
 privateKey=$(cat scratch) 
 publicKey=$(cat scratch.pub) 
 json="{\"keyinfo\":{\"privateKey\":\"$privateKey\",\"publicKey\":\"$publicKey\"}}" 
-echo "$json" > ../sshkeys.json
+echo "$json" > sshkeys.json
